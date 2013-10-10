ceed3577c370652a1adb1ad48ac32d09c2d4bf35
Noah Carnahan <noah.carnahan@gmail.com>
2013-10-09 22:57:03 -0500

    Moved doc strings. Added an exception. Refactored the if statement in getFeatures slightly. Fixed a bug in the sentence case.

--- feature_extractor.py
+++ feature_extractor.py
@@ -6,12 +6,14 @@
 import nltk, re
 
 class StylometricFeatureEvaluator:
+
 	def __init__(self, filepath):
 		self.setDocument(filepath)
 		self.punctuation_re = re.compile(r'[\W\d]+', re.UNICODE) # slightly modified from nltk source
 	
-	''' Reads in the file and constructs a list of words, sentences, and paragraphs from it. '''
+	
 	def setDocument(self, filepath):
+		''' Reads in the file and constructs a list of words, sentences, and paragraphs from it. '''
 		f = open(filepath, 'r')
 		self.input_file = f.read()
 		f.close()
@@ -28,37 +30,42 @@ class StylometricFeatureEvaluator:
 	def parseSentences(self, text):
 		return nltk.sent_tokenize(text)
 	
-	''' Returns a list of extracted stylometric features from the specified chunk of the document.
-	    The start and end indices use the same logic as indexing a string or list in Python.
-	    The atom_type parameter specifies what your start_index and end_index parameters refer to.
-	    For example, if you wanted to query the stylometric features for sentences 0 through 4, you 
-	    would call this function as getFeatures(0, 4, "sentence"). '''
 	def getFeatures(self, start_index, end_index, atom_type):
+		''' Returns a list of extracted stylometric features from the specified chunk of the document.
+	    	The start and end indices use the same logic as indexing a string or list in Python.
+		    The atom_type parameter specifies what your start_index and end_index parameters refer to.
+		    For example, if you wanted to query the stylometric features for sentences 0 through 4, you 
+	    	would call this function as getFeatures(0, 4, "sentence"). '''
+	    	
 		if atom_type == 'char':
 			input_file_chunk = self.input_file[start_index : end_index]
-			avg_word_length = self.averageWordLength(self.parseWords(input_file_chunk))
-			avg_sentence_length = self.averageSentenceLength(self.parseSentences(input_file_chunk))
+			word_chunk = self.parseWords(input_file_chunk)
+			sentence_chunk = self.parseSentences(input_file_chunk)
 		elif atom_type == 'word':
 			word_chunk = self.words[start_index : end_index]
-			avg_word_length = self.averageWordLength(word_chunk)
-			avg_sentence_length = self.averageSentenceLength(self.parseSentences(" ".join(word_chunk)))
+			sentence_chunk = self.parseSentences(" ".join(word_chunk))
+			# This method of building sentences might not be ideal. "$3.88" will be parsed
+			# into ["$","3",".","88"] then joined back together as "$ 3 . 88". Unsure of
+			# what other situations this could be problematic for...
 		elif atom_type == 'sentence':
 			sentence_chunk = self.sentences[start_index : end_index]
-			avg_word_length = self.averageWordLength(self.parseWords(" ".join(sentence_chunk)))
-			avg_sentence_length = self.averageSentenceLength(self.parseSentences(sentence_chunk))
+			word_chunk = self.parseWords(" ".join(sentence_chunk))
 		elif atom_type == 'paragraph':
 			paragraph_chunk = self.paragraphs[start_index : end_index]
-			avg_word_length = self.averageWordLength(self.parseWords(" ".join(paragraph_chunk)))
-			avg_sentence_length = self.averageSentenceLength(self.parseSentences(" ".join(paragraph_chunk)))
+			word_chunk = self.parseWords(" ".join(paragraph_chunk))
+			sentence_chunk = self.parseSentences(" ".join(paragraph_chunk))
 		else:
-			avg_word_length = -1
-			avg_sentence_length = -1
+			raise ValueError("atom_type string must be 'char', 'word', 'sentence' or 'paragraph', not '" + str(atom_type) + "'.")
+
+		avg_word_length = self.averageWordLength(word_chunk)
+		avg_sentence_length = self.averageSentenceLength(sentence_chunk)
 		
 		return [avg_word_length, avg_sentence_length]
 	
-	''' Returns the average word length for the given list of words. 
-	    Doesn't count puncuation as words. '''
+
 	def averageWordLength(self, words):
+		''' Returns the average word length for the given list of words. 
+	    	Doesn't count puncuation as words. '''
 		total = 0
 		num_words = 0
 		for word in words:
@@ -68,8 +75,9 @@ class StylometricFeatureEvaluator:
 				num_words += 1
 		return float(total) / max(num_words, 1) # if there are no legitimate words, just set denominator to 1 to avoid division by 0
 		
-	''' Returns the average words-per-sentence for the given list of sentences. '''
+	
 	def averageSentenceLength(self, sentences):
+		''' Returns the average words-per-sentence for the given list of sentences. '''
 		total = 0
 		num_sentences = 0
 		for sentence in sentences:
@@ -80,8 +88,9 @@ class StylometricFeatureEvaluator:
 			total += num_words
 		return  float(total) / max(len(sentences), 1) # avoid division by 0
 	
-	''' Returns true if the given word is just punctuation. '''
+	
 	def _is_punctuation(self, word):
+		''' Returns true if the given word is just punctuation. '''
 		match_obj = re.match(self.punctuation_re, word)
 		return match_obj and len(match_obj.group()) == len(word)
 	
@@ -92,5 +101,8 @@ class StylometricFeatureEvaluator:
 		print
 		print 'Extracted Stylometric Feature Vector: <avg_word_length, avg_words_in_sentence>'		
 		print self.getFeatures(0, len(self.input_file), "char")
-		
-StylometricFeatureEvaluator("foo.txt").test()
+		print self.getFeatures(0, 2, "sentence")
+		print self.getFeatures(0, 2, "bad")
+
+if __name__ == "__main__":
+	StylometricFeatureEvaluator("foo.txt").test()
