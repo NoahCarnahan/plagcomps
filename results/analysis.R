8a3f9d824293a0811ac041cde6995717bb85b378
Nick Jones <nrjones8@gmail.com>
2013-10-29 10:36:44 -0500

    Added some plots of our first training data

--- results/analysis.R
+++ results/analysis.R
@@ -1,31 +1,71 @@
-library(plyr)
+library(ggplot2)
+library(Hmisc)
 
-one_summary <- function(df, feature) {
-	only_feature <- df[which(df[, feature] == 1), ]
-	print(head(only_feature))
-	
-	hist(only_feature$accuracy)
+plot_one_metric <- function(df, metric) {
+  g <- ggplot(combined, aes_string(x = 'feature', y = metric)) + 
+    geom_boxplot() + 
+    scale_x_discrete('Feature(s) Used', labels = c('averageSentenceLength' = 'Sent. Len.',
+                                                   'averageWordLength' = 'Word Len.',
+                                                   'get_avg_word_frequency_class' = 'Avg Word Freq Class',
+                                                   'get_punctuation_percentage' = 'Punc. %',
+                                                   'get_stopword_percentage' = 'StopWord %',
+                                                   'all' = 'All')) +
+    scale_y_continuous(capitalize(metric)) +
+    ggtitle(capitalize(metric))
+  
+  print(g)  
 }
 
 
-results <- read.csv('results/first_training_set_test.csv', head = T)
+set_feature_col <- function(df) {
+	df$feature <- 'other'
+	df[which(results$averageWordLength == 1), 'feature'] <- 'averageWordLength'
+	df[which(results$averageSentenceLength == 1), 'feature'] <- 'averageSentenceLength'		
+	df[which(results$get_avg_word_frequency_class == 1), 'feature'] <- 'get_avg_word_frequency_class'		
+	df[which(results$get_punctuation_percentage == 1), 'feature'] <- 'get_punctuation_percentage'
+	df[which(results$get_stopword_percentage == 1), 'feature'] <- 'get_stopword_percentage'		
+	
+	return(df)
+}
 
-results$accuracy <- (results$true_pos + results$true_neg) / (results$true_pos + results$true_neg + results$false_pos + results$false_neg)
-summary(results$accuracy)
+set_accuracy_spec_cols <- function(df) {
+  df$accuracy <- (df$true_pos + df$true_neg) / (df$true_pos + df$true_neg + df$false_pos + df$false_neg)
+  df$precision <- df$true_pos / (df$true_pos + df$false_pos)
+  # Note that recall can't really be definied for a non-plagarized text, since the number
+  # of true pos = 0 and number of false neg = 0
+  df$recall <- df$true_pos / (df$true_pos + df$false_neg)
+  
+  return(df)
+}
 
-one_summary(results, 'averageWordLength')
+results <- read.csv('results/initial_results/first_training_set_test.csv', head = T)
+all_features <- read.csv('results/initial_results/full_set_first_training_test.csv', head = T)
 
+# Add columns for accuracy, precision, recall
+results <- set_accuracy_spec_cols(results)
+all_features <- set_accuracy_spec_cols(all_features)
 
-word_length <- results[which(results$averageWordLength == 1), ]
-summary(word_length$accuracy)
+# Add a column showing which feature was used
+results <- set_feature_col(results)
+all_features$feature <- 'all'
 
-sent_length <- results[which(results$averageSentenceLength == 1), ]
+# Combine the single-feature results with the using-all-features results
+combined <- merge(results, all_features, all = T)
 
-word_freq <- results[which(results$get_avg_word_frequency_class) == 1, ]
+# Accuracy
+plot_one_metric(combined, 'accuracy')
+# precision
+plot_one_metric(combined, 'precision')
+# recall
+plot_one_metric(combined, 'recall')
 
-punct_pct <- results[which(results$get_punctuation_percentage == 1), ]
+g <- ggplot(combined, aes(docname, accuracy, color = feature)) +
+  geom_point()
+print(g)
 
-stopword_pct <- results[which(results$get_stopword_percentage == 1), ]
+# word_length <- results[which(results$averageWordLength == 1), ]
+# sent_length <- results[which(results$averageSentenceLength == 1), ]
+# word_freq <- results[which(results$get_avg_word_frequency_class) == 1, ]
+# punct_pct <- results[which(results$get_punctuation_percentage == 1), ]
+# stopword_pct <- results[which(results$get_stopword_percentage == 1), ]
 
-
-names(results)
---
+++ results/initial_results/readme.txt
@@ -0,0 +1,14 @@
+Data generated using the first 120 files from our training set (contained in training_set_files.txt).
+Each of the following features was used in isolation as the only feature:
+- average word length
+- average sentence length
+- average word frequency class
+- punctuation percentage
+- stopword percentage
+
+The results for these trials are stored first_training_set_test.csv.
+
+Finally, we also tested the first 120 files using all features at once.
+The results from these trials are stored in full_set_first_training_test.csv.
+
+ 
