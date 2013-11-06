3778d1a580a7be2a351279b9def0d7abec2299bc
Noah Carnahan <carnahan@carleton.edu>
2013-11-05 20:40:41 -0600

    Trying to figure out how SQLAlchemy works... still working on it

--- bar.py
+++ bar.py
@@ -1,15 +1,24 @@
 # Should there be document objects that tell us things like how many instances of plagiarism
 # they have or how long they are?
 
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Sequence, Integer, String, Float
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+Base = declarative_base()
+
 class ProcessedDocument(Base):
     __tablename__ = "processed_documents"
 
     id =                Column(Integer, Sequence("processed_document_id_seq"), primary_key=True)
     name =              Column(String)
-    features =          Column(sqlalchemy.dialects.postgresql.ARRAY(String)) # http://docs.sqlalchemy.org/en/rel_0_8/core/types.html
+    features =          Column(ARRAY(String)) # http://docs.sqlalchemy.org/en/rel_0_8/core/types.html
     atom_type =         Column(String)
-    feature_vectors =   Column(sqlalchemy.dialects.postgresql.ARRAY(sqlalchemy.dialects.postgresql.ARRAY(Float))) 
-    clusters =          relationship("ClusteredFeatures", order_by="ClusteredFeatures.id", backref="processed_document")
+    feature_vectors =   Column(ARRAY(Float)) # Handles multi dimensionality automatically  
+    clusters =          relationship("ClusteredFeatures", backref="processed_document")
     # http://docs.sqlalchemy.org/en/rel_0_9/orm/tutorial.html#building-a-relationship
 
     def __init__(self, doc_name, features, atom_type, feature_vectors):
@@ -25,16 +34,17 @@ class ClusteredFeatures(Base):
     '''
     Represents a clustering of the feature vectors in self.processed_document.
     '''
-
-    # Do we need both document and document_id?
-    id =                Column(Integer, Sequence("clustered_features_id_seq"), primary_key=True)
-    processed_document= relationship("ProcessedDocument", backref=backref('clusters', order_by=id))
-    document_id =       Column(Integer, ForeignKey('ProcessedDocument.id'))
-    clustering_type =   Column(String)
-    plag_centroid =     Column(sqlalchemy.dialects.postgresql.ARRAY(Float))
-    notplag_centroid =  Column(sqlalchemy.dialects.postgresql.ARRAY(Float))
+    __table__ = "clustered_features"
     
-    def __init__(self, doc, clustering_type, plag_c, notplag_c)
+    # Do we need both document and document_id? Is processed_document created automatically? 
+    id =                Column(Integer, Sequence("clustered_features_id_seq"), primary_key=True)
+    #processed_document= relationship("ProcessedDocument", backref=backref('clusters', order_by=id))
+    processed_document_id = Column(Integer, ForeignKey('processed_document.id'))
+    clustering_type =   Column(String)
+    plag_centroid =     Column(ARRAY(Float))
+    notplag_centroid =  Column(ARRAY(Float))
+    
+    def __init__(self, doc, clustering_type, plag_c, notplag_c):
         self.processed_document = doc
         self.clustering_type = clustering_type
         self.plag_centroid = plag_c
@@ -44,10 +54,13 @@ class ClusteredFeatures(Base):
         return "<ClusteredFeatures('%s','%s')>" % (self.processed_document, self.clustering_type)
 
 class PerformanceEvaluation(Base):
-    id =                Column(Integer, Sequence("performance_evaluation_id_seq", primary_key=True)
+
+    __table__ = "performace_evaluations"
+    
+    id =                Column(Integer, Sequence("performance_evaluation_id_seq", primary_key=True))
     clustered_features= relationship("ClusteredFeatures")
-    roc_figure        = Column(String)
-    au_roc            = Column(String)
+    roc_figure =        Column(String)
+    au_roc =            Column(String)
     
     def __init__(self, clustered_features, roc_fig_path, au_roc):
         self.clustered_features = clustered_features
