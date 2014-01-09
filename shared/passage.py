46c02f4ea75489dcc6e736a5c08aa3ca4ae59e4d
Marcus Huderle <huderlem@carleton.edu>
2014-01-08 21:53:55 -0600

    Added ExtrinsicPassage.

--- shared/passage.py
+++ shared/passage.py
@@ -66,3 +66,48 @@ class IntrinsicPassage(Passage):
 		Sets how confident we are this passage was plagiarized
 		'''
 		self.plag_confidence = confidence
+
+class ExtrinsicPassage(Passage):
+    '''
+    Stores additional fields for an extrinsic passage, namely 
+    <atom_type> ('paragraph')
+    and <features> (and dictionary mapping feature -> observed value for that feature)
+    '''
+    def __init__(self, char_index_start, char_index_end, text, atom_type, fingerprint):
+        Passage.__init__(self, char_index_start, char_index_end, text)
+        self.atom_type = atom_type
+        self.fingerprint = fingerprint
+        self.plag_confidence = None
+
+    def __str__(self):
+        return 'Text: ' + self.text + \
+        '\nFingerprint: ' + str(self.features) +  \
+        '\nPlag Confidence: ' + str(self.plag_confidence) + \
+        '\n------------'
+
+    def to_json(self):
+        '''
+        Returns a dictionary representation of the passage
+        '''
+        json_rep = {
+            'atom_type' : self.atom_type,
+            'char_index_start' : self.char_index_start,
+            'char_index_end' : self.char_index_end,
+            'text' : self.text,
+            'fingerprint' : self.fingerprint
+        }
+        return json_rep
+
+    def to_html(self):
+        '''
+        Returns an HTML representation of this passage's fingerprint, for use 
+        by the plagapp front-end
+        '''
+        html = '<p>Plag. conf.</p>'
+        html += '<p>%s</p>' % str(self.plag_confidence) 
+        html += '<hr size = "10"'
+
+        for minutia in self.fingerprint:
+            html += '<p>%d</p>' % minutia
+            html += '<hr size = "10"'
+        return html
