7d524b8a006e837ec42764969ea984fdfb597e51
Nick Jones <nrjones8@gmail.com>
2014-01-13 22:33:36 -0600

    Added some frontend supporT

--- shared/passage.py
+++ shared/passage.py
@@ -11,6 +11,11 @@ class Passage:
 		self.char_index_start = char_index_start
 		self.char_index_end = char_index_end
 		self.text = text
+		self.plag_spans = []
+
+	def add_plag_span(self, span):
+		self.plag_spans.append(span)
+
 
 class PassageWithGroundTruth(Passage):
 	'''
@@ -64,13 +69,66 @@ class IntrinsicPassage(Passage):
 		return json_rep
 
 
+	def get_relative_plag_spans(self):
+		'''
+		Returns the span, relative to the text stored in <self.text>,
+		of the plagiarized span 
+		'''
+		rel_spans = []
+
+		for span in self.plag_spans:
+			start = span[0] - self.char_index_start
+			end = span[1] - self.char_index_start
+			
+			rel_spans.append((start, end))
+
+		# Sorts by first element by default
+		rel_spans.sort()
+
+		return rel_spans
+
+	def format_text_with_underlines(self):
+		'''
+		Returns HTML for <self.text> including markup to underline 
+		plagiarized passages
+		'''
+		rel_spans = self.get_relative_plag_spans()
+		html = ''
+		last_end = 0
+
+		for cur_span in rel_spans:
+			cur_start, cur_end = cur_span
+
+			html += self.text[last_end : cur_start]
+			html += '<span style="text-decoration: underline;">'
+			html += self.text[cur_start : cur_end]
+			html += '</span>'
+			last_end = cur_end
+
+		html += self.text[last_end:]
+	
+		return html
+	
 	def to_html(self):
 		'''
 		Returns an HTML representation of this passage's features, for use 
 		by the plagapp front-end
 		'''
-		html = '<p>Plag. conf.</p>'
+		
+		html = '<p>Span</p>'
+		html += '<p>%i, %i</p>' % (self.char_index_start, self.char_index_end)
+		html += '<hr size = "10"'
+
+		html += '<p>Plag. conf.</p>'
 		html += '<p>%s</p>' % str(self.plag_confidence) 
+		html += '<hr size = "10"'
+
+		html += '<p>PLAG SPAN</p>'
+		if len(self.plag_spans) > 0:
+			for sp in self.plag_spans:
+				html += '<p>(%i, %i)</p>' % sp
+		else:
+			html += '<p>No plag!</p>'
 		html += '<hr size = "10"'
 
 		for feat, val in self.features.iteritems():
@@ -87,46 +145,46 @@ class IntrinsicPassage(Passage):
 		self.plag_confidence = confidence
 
 class ExtrinsicPassage(Passage):
-    '''
-    Stores additional fields for an extrinsic passage, namely 
-    <atom_type> ('paragraph')
-    and <features> (and dictionary mapping feature -> observed value for that feature)
-    '''
-    def __init__(self, char_index_start, char_index_end, text, atom_type, fingerprint):
-        Passage.__init__(self, char_index_start, char_index_end, text)
-        self.atom_type = atom_type
-        self.fingerprint = fingerprint
-        self.plag_confidence = None
+	'''
+	Stores additional fields for an extrinsic passage, namely 
+	<atom_type> ('paragraph')
+	and <features> (and dictionary mapping feature -> observed value for that feature)
+	'''
+	def __init__(self, char_index_start, char_index_end, text, atom_type, fingerprint):
+		Passage.__init__(self, char_index_start, char_index_end, text)
+		self.atom_type = atom_type
+		self.fingerprint = fingerprint
+		self.plag_confidence = None
 
-    def __str__(self):
-        return 'Text: ' + self.text + \
-        '\nFingerprint: ' + str(self.features) +  \
-        '\nPlag Confidence: ' + str(self.plag_confidence) + \
-        '\n------------'
+	def __str__(self):
+		return 'Text: ' + self.text + \
+		'\nFingerprint: ' + str(self.features) +  \
+		'\nPlag Confidence: ' + str(self.plag_confidence) + \
+		'\n------------'
 
-    def to_json(self):
-        '''
-        Returns a dictionary representation of the passage
-        '''
-        json_rep = {
-            'atom_type' : self.atom_type,
-            'char_index_start' : self.char_index_start,
-            'char_index_end' : self.char_index_end,
-            'text' : self.text,
-            'fingerprint' : self.fingerprint
-        }
-        return json_rep
+	def to_json(self):
+		'''
+		Returns a dictionary representation of the passage
+		'''
+		json_rep = {
+			'atom_type' : self.atom_type,
+			'char_index_start' : self.char_index_start,
+			'char_index_end' : self.char_index_end,
+			'text' : self.text,
+			'fingerprint' : self.fingerprint
+		}
+		return json_rep
 
-    def to_html(self):
-        '''
-        Returns an HTML representation of this passage's fingerprint, for use 
-        by the plagapp front-end
-        '''
-        html = '<p>Plag. conf.</p>'
-        html += '<p>%s</p>' % str(self.plag_confidence) 
-        html += '<hr size = "10"'
+	def to_html(self):
+		'''
+		Returns an HTML representation of this passage's fingerprint, for use 
+		by the plagapp front-end
+		'''
+		html = '<p>Plag. conf.</p>'
+		html += '<p>%s</p>' % str(self.plag_confidence) 
+		html += '<hr size = "10"'
 
-        for minutia in self.fingerprint:
-            html += '<p>%d</p>' % minutia
-            html += '<hr size = "10"'
-        return html
+		for minutia in self.fingerprint:
+			html += '<p>%d</p>' % minutia
+			html += '<hr size = "10"'
+		return html
