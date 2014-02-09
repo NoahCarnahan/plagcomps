5493c0d33d6f0c6a48bc11ba50f9d234dfa084d9
Nick Jones <nrjones8@gmail.com>
2014-02-09 13:29:58 -0600

    Finished filling out new extrinsic database, began population

--- extrinsic/two_table_fp_storage.py
+++ extrinsic/two_table_fp_storage.py
@@ -1,5 +1,6 @@
 import psycopg2
 import os
+
 import fingerprint_extraction
 from ..dbconstants import username, password, dbname
 from ..shared.util import ExtrinsicUtility
@@ -12,7 +13,7 @@ DEV_MODE = True
 
 '''
 Tables created with:
-CREATE TABLE dev_passage_fingerprints (
+CREATE TABLE passage_fingerprints (
     id serial,
     doc_name text,
     doc_path text,
@@ -27,92 +28,96 @@ CREATE TABLE dev_passage_fingerprints (
     hash_len integer
 );
 
-CREATE TABLE dev_hash_table (
+CREATE TABLE hash_table (
     fingerprint_id integer,
     hash_value integer
 );
 '''
 
-def populate_database(files, method, n, k, atom_type, hash_len):
+def populate_database(files, method, n, k, atom_type, hash_len, check_for_duplicate=True):
     '''
     Example usages:
     srs, sus = ExtrinsicUtility().get_training_files(n=10)
     populate_database(srs+sus, "kth_in_sent", n, k, "paragraph", 10000)
-    TODO be smart and don't overwrite fingerprints we've already calculated!
+    
+    If <check_for_duplicate>, a query for a duplicate row is made before creating a new row 
+    and fingerprinting a given document/set of parameters. In other words, we check for the 
+    exact row before creating it (and all of its hashes). This doubles the number of queries
+    made to the passage_fingerprints table -- if we are running a population script for the first
+    time, we can set this flag to be False to speed things up. BUT we must be careful later on
+    not to create duplicates (which is why its default is to make the extra check)
     
     '''
+    with psycopg2.connect(user = username, password = password, database = dbname.split("/")[1], host="localhost", port = 5432) \
+            as conn:
+        conn.autocommit = True
         
-    conn = psycopg2.connect(user = username, password = password, database = dbname.split("/")[1], host="localhost", port = 5432)
-    conn.autocommit = True
-    
-    for abs_path in files:
-    
-        # Get doc_name, doc_path, doc_xml_path, and is_source
-        doc_name, doc_path, doc_xml_path, is_source =  _get_doc_metadata(abs_path)
-        # open the file, and tokenize it.
-        # Build atom_texts list
-        f = open(doc_path, "r")
-        text = f.read()
-        f.close()
-        if atom_type == "full":
-            atom_texts = [text]
-        elif atom_type == "paragraph":
-            paragraph_spans = tokenize(text, atom_type)
-            atom_texts = [text[start:end] for start, end in paragraph_spans]
-        else:
-            raise ValueError("Invalid atom_type! Only 'full' and 'paragraph' are allowed.")
+        for doc_num, abs_path in enumerate(files):
         
-        # Initialize a string to write to the copy file.
-        copy_file_string = ""
-        
-        cur = conn.cursor()
-        for atom_number in xrange(len(atom_texts)):
-            
-            
-            # Table names aren't permitted to be parameterized -- have to pass them explicitly
-            # Set DEV_MODE = True to use a development version of the fingerprint/hash table
-            if DEV_MODE:
-                passage_query = """INSERT INTO dev_passage_fingerprints
-                                (doc_name, doc_path, doc_xml_path, atom_number, is_source, atom_type, method, n, k, hash_len)
-                                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id;"""
+            print 'Started working on doc %i of %i' % (doc_num, len(files))
+            # Get doc_name, doc_path, doc_xml_path, and is_source
+            doc_name, doc_path, doc_xml_path, is_source =  _get_doc_metadata(abs_path)
+            # open the file, and tokenize it.
+            # Build atom_texts list
+            f = open(doc_path, "r")
+            text = f.read()
+            f.close()
+            if atom_type == "full":
+                atom_texts = [text]
+            elif atom_type == "paragraph":
+                paragraph_spans = tokenize(text, atom_type)
+                atom_texts = [text[start:end] for start, end in paragraph_spans]
             else:
-                passage_query = """INSERT INTO passage_fingerprints
-                                (doc_name, doc_path, doc_xml_path, atom_number, is_source, atom_type, method, n, k, hash_len)
-                                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id;"""
-
-            passage_args = (doc_name, doc_path, doc_xml_path, atom_number, is_source, atom_type, method, n, k, hash_len)
-            # Insert the passage into the database and get its id
-            cur.execute(passage_query, passage_args)
-            passage_id = str(cur.fetchone()[0])
+                raise ValueError("Invalid atom_type! Only 'full' and 'paragraph' are allowed.")
             
-            # Fingerprint the passage
-            extractor = fingerprint_extraction.FingerprintExtractor(hash_len)
-            hash_values = extractor.get_fingerprint(atom_texts[atom_number], n, method, k)
+            # Initialize a string to write to the copy file.
+            copy_file_string = ""
             
-            # Add rows to the copy_file_string
-            for hash_value in hash_values:
-                copy_file_string += passage_id + "\t" + str(hash_value) + "\n"
+            with conn.cursor() as cur:
+                for atom_number in xrange(len(atom_texts)):
+                    
+                    # Only make the duplicate-check query if checking for duplicates
+                    # I'd love to use a Ruby `unless` statement here. Oh well...
+                    if check_for_duplicate and _row_already_exists(doc_path, atom_number, atom_type, method, n, k, hash_len):
+                        print (doc_path, atom_number, atom_type, method, n, k, hash_len), 'already existed! Skipped it.'
+                        continue
+                    else:
+                        # Table names aren't permitted to be parameterized -- have to pass them explicitly
+                        # Set DEV_MODE = True to use a development version of the fingerprint/hash table
+                        if DEV_MODE:
+                            passage_query = """INSERT INTO dev_passage_fingerprints
+                                            (doc_name, doc_path, doc_xml_path, atom_number, is_source, atom_type, method, n, k, hash_len)
+                                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id;"""
+                        else:
+                            passage_query = """INSERT INTO passage_fingerprints
+                                            (doc_name, doc_path, doc_xml_path, atom_number, is_source, atom_type, method, n, k, hash_len)
+                                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id;"""
 
-        # Number of lines about to be inserted into DB
-        num_rows_to_insert = copy_file_string.count('\n')
-        _display_one_doc_debug(doc_name, atom_type, len(atom_texts), num_rows_to_insert)
+                        passage_args = (doc_name, doc_path, doc_xml_path, atom_number, is_source, atom_type, method, n, k, hash_len)
+                        # Insert the passage into the database and get its id
+                        # TODO is any executemany() call faster?
+                        cur.execute(passage_query, passage_args)
+                        passage_id = str(cur.fetchone()[0])
+                        
+                        # Fingerprint the passage
+                        extractor = fingerprint_extraction.FingerprintExtractor(hash_len)
+                        hash_values = set(extractor.get_fingerprint(atom_texts[atom_number], n, method, k))
+                        
+                        # Add rows to the copy_file_string
+                        for hash_value in hash_values:
+                            copy_file_string += passage_id + "\t" + str(hash_value) + "\n"
 
-        # Writes all hashes to file, then bulk inserts to DB
-        # NOTE THAT the cursor is NOT closed by the below function. It's cleaned up at the 
-        # end of the populate_database() function
-        _copy_and_write_to_hash_table(copy_file_string, cur)
+                # Number of lines about to be inserted into DB
+                num_rows_to_insert = copy_file_string.count('\n')
+                _display_one_doc_debug(doc_name, atom_type, len(atom_texts), num_rows_to_insert)
 
-    cur.close()
-    conn.close()
-
+                # Writes all hashes to file, then bulk inserts to DB
+                _copy_and_write_to_hash_table(copy_file_string, cur)
 
 def get_passage_fingerprint(full_path, passage_num, method, n, k, atom_type, hash_len):
     '''
     Return the fingerprint (list of hash values) of the <passage_num>th passage in <file_name>
     using parameters <method>, <n>, <k>
-
-    TODO clean up output. Returning a list like this:
-    [(3160,), (2080,), (584,), (1941,) ...]
     '''
     # With statement cleans up the connection
     with psycopg2.connect(user = username, password = password, database = dbname.split("/")[1], host="localhost", port = 5432) \
@@ -132,16 +137,16 @@ def get_passage_fingerprint(full_path, passage_num, method, n, k, atom_type, has
                         dev_passage_fingerprints.atom_type = %s AND
                         dev_passage_fingerprints.hash_len = %s;'''
         else:
-            fp_query = '''SELECT dev_hash_table.hash_value FROM dev_hash_table, dev_passage_fingerprints
+            fp_query = '''SELECT hash_table.hash_value FROM hash_table, passage_fingerprints
                         WHERE
-                        dev_passage_fingerprints.id = dev_hash_table.fingerprint_id AND
-                        dev_passage_fingerprints.doc_path = %s AND
-                        dev_passage_fingerprints.atom_number = %s AND
-                        dev_passage_fingerprints.method = %s AND
-                        dev_passage_fingerprints.n = %s AND
-                        dev_passage_fingerprints.k = %s AND
-                        dev_passage_fingerprints.atom_type = %s AND
-                        dev_passage_fingerprints.hash_len = %s;'''
+                        passage_fingerprints.id = hash_table.fingerprint_id AND
+                        passage_fingerprints.doc_path = %s AND
+                        passage_fingerprints.atom_number = %s AND
+                        passage_fingerprints.method = %s AND
+                        passage_fingerprints.n = %s AND
+                        passage_fingerprints.k = %s AND
+                        passage_fingerprints.atom_type = %s AND
+                        passage_fingerprints.hash_len = %s;'''
         
         fp_args = (full_path, passage_num, method, n, k, atom_type, hash_len)
 
@@ -150,15 +155,15 @@ def get_passage_fingerprint(full_path, passage_num, method, n, k, atom_type, has
             cur.execute(fp_query, fp_args)
             fingerprint = cur.fetchall()
 
-        return fingerprint
+        # Format into nicer list
+        clean_fp = [hsh[0] for hsh in fingerprint]
+        
+        return clean_fp
 
 def get_passage_ids_with_hash(target_hash_value, method, n, k, atom_type, hash_len):
     '''
     Returns a list of IDs corresponding to rows in the <passage_fingerprints> table which
     contain <target_hash_value> as part of its fingerprint. 
-
-    TODO clean up output. Returning a list like this:
-    [(7,), (103,), (3774,), (4083,)...]
 
     TODO this returns all IDs, including the one used to query. Don't forget to filter this out!
     '''
@@ -192,7 +197,9 @@ def get_passage_ids_with_hash(target_hash_value, method, n, k, atom_type, hash_l
             cur.execute(reverse_query, reverse_args)
             matching_passage_ids = cur.fetchall()
 
-    return matching_passage_ids
+    clean_ids = [passid[0] for passid in matching_passage_ids]
+
+    return clean_ids
 
 def _copy_and_write_to_hash_table(copy_file_string, curs):
     '''
@@ -231,6 +238,41 @@ def _get_doc_metadata(abs_path):
 
     return doc_name, doc_path, doc_xml_path, is_source      
             
+def _row_already_exists(doc_path, atom_number, atom_type, method, n, k, hash_len):
+    '''
+    Checks if a row with the provided parameters already exists by querying the DB. Used to make 
+    sure we don't overwrite/re=fingerprint documents
+    '''
+    with psycopg2.connect(user = username, password = password, database = dbname.split("/")[1], host="localhost", port = 5432) \
+            as conn:
+        if DEV_MODE:
+            existence_query = '''SELECT COUNT(*) FROM dev_passage_fingerprints
+                                WHERE
+                                doc_path = %s AND
+                                atom_number = %s AND
+                                method = %s AND
+                                n = %s AND
+                                k = %s AND
+                                atom_type = %s AND
+                                hash_len = %s;'''
+        else:
+            existence_query = '''SELECT COUNT(*) FROM passage_fingerprints
+                                WHERE
+                                doc_path = %s AND
+                                atom_number = %s AND
+                                method = %s AND
+                                n = %s AND
+                                k = %s AND
+                                atom_type = %s AND
+                                hash_len = %s;'''
+        existence_args = (doc_path, atom_number, method, n, k, atom_type, hash_len)
+
+        with conn.cursor() as cur:
+            cur.execute(existence_query, existence_args)
+            num_existing_rows = cur.fetchall()
+
+    return num_existing_rows[0][0] > 0
+
 def _display_one_doc_debug(doc_name, atom_type, num_atoms, num_rows_to_insert):
     '''
     Print data about document being processed
@@ -253,7 +295,7 @@ def _test():
     atom_type = 'paragraph'
     hash_len = 10000
 
-    srs, sus = ExtrinsicUtility().get_training_files(n=10)
+    srs, sus = ExtrinsicUtility().get_training_files(n=12)
     populate_database(srs+sus, method, n, k, atom_type, hash_len)
 
 def _test_get_fp_query():
@@ -284,7 +326,7 @@ def _test_reverse_lookup():
     k = 3
     atom_type = 'paragraph'
     hash_len = 10000
-    srcs, sus = ExtrinsicUtility().get_training_files(n=2)
+    srcs, sus = ExtrinsicUtility().get_training_files(n=1)
 
     for s in sus:
         print 'Fingerprint for', s
@@ -294,13 +336,36 @@ def _test_reverse_lookup():
         num_atoms = 1 if atom_type == 'full' else len(tokenize(text, atom_type))
         
         for atom_num in xrange(num_atoms):
-            passage_fp = [hsh[0] for hsh in set(get_passage_fingerprint(s, atom_num, method, n, k, atom_type, hash_len))]
+            passage_fp = set(get_passage_fingerprint(s, atom_num, method, n, k, atom_type, hash_len))
             for single_hash in passage_fp:
                 matching_pass_ids = get_passage_ids_with_hash(single_hash, method, n, k, atom_type, hash_len)
-                print single_hash, matching_pass_ids
+                print 'Hash %i found in:' % (single_hash)
+                print matching_pass_ids
         print '-'*40
 
+def _populate_variety_of_params():
+    srs, sus = ExtrinsicUtility().get_training_files(n=100)
+    hash_len = 10000
+    # method, n, k, atom_type
+    populate_database(sus+srs, "anchor", 5, 0, "paragraph", hash_len, check_for_duplicate=False)
+    populate_database(sus+srs, "anchor", 3, 0, "paragraph", hash_len, check_for_duplicate=False)
+    print 'Done with anchor params!'
+
+    populate_database(sus+srs, "kth_in_sent", 5, 5, "paragraph", hash_len, check_for_duplicate=False)
+    populate_database(sus+srs, "kth_in_sent", 5, 3, "paragraph", hash_len, check_for_duplicate=False)
+    populate_database(sus+srs, "kth_in_sent", 3, 5, "paragraph", hash_len, check_for_duplicate=False)
+    populate_database(sus+srs, "kth_in_sent", 3, 3, "paragraph", hash_len, check_for_duplicate=False)
+    print 'Done with kth_in_sent params!'
+
+    populate_database(sus+srs, "full", 5, 0, "paragraph", hash_len, check_for_duplicate=False)
+    populate_database(sus+srs, "full", 3, 0, "paragraph", hash_len, check_for_duplicate=False)
+    #populate_db(sus+srs, "winnow-k", 5, 5, "paragraph")
+    #populate_db(sus+srs, "winnow-k", 5, 3, "paragraph")
+    #populate_db(sus+srs, "winnow-k", 3, 5, "paragraph")
+    #populate_db(sus+srs, "winnow-k", 3, 3, "paragraph")
+
 if __name__ == "__main__":
+    print 'DEV_MODE is set to', DEV_MODE
+    #_populate_variety_of_params()
     _test_get_fp_query()
-    #_test_reverse_lookup()
     #_test()
